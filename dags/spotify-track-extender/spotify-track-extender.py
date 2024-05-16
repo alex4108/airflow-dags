006c6e72c4b542a8e67e244b88775167aef707d2
Alex Schittko <alex4108@live.com>
2024-05-16 12:46:02 +0000

    V close?

--- dags/spotify-track-extender/spotify-track-extender.py
+++ dags/spotify-track-extender/spotify-track-extender.py
@@ -8,7 +8,7 @@ from airflow.models import Variable
 
 import requests
 import base64
-
+import json
 
 default_args = {
     'owner': 'airflow',
@@ -107,7 +107,8 @@ def call_spotify_api_and_save(url, **kwargs):
     print(f"exec fetch: {url}")
     track_details = fetch_track_details(url)
     print("got detailed!")
-    insert_query = f"INSERT INTO spotify_track_details (url, track_details) VALUES ('{url}', '{track_details}')"
+    track_details_str = json.dumps(track_details)
+    insert_query = f"INSERT INTO spotify_track_details (url, track_details) VALUES ('{url}', '{track_details_str}')"
     id = get_spotify_track_id(url)
     insert_task = PostgresOperator(
         task_id=f"insert_track_details_{id}",
